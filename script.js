8741ecf88e99c8fc262190aa6b5668b8e451f678
Av-rae <rm99011@fiap.com.br>
2023-03-07 20:45:56 -0300

    fixed some errors

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-for(var fIz > 1, fIz < 101; fIz++ ) {
+for(var fIz = 1; fIz < 101; fIz++ ) {
 	if(fIz % 15 == 0) console.log('fizzbuzz');
 	else if (fIz % 5 == 0) console.log('fizz');
 	else if (fIz % 3 == 0) console.log('buzz');
